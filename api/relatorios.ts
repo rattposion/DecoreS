44019aeb7be20d7198cf52d68b05e051f43ae9ab
Wesley <142677140+rattposion@users.noreply.github.com>
2025-06-06 13:41:19 -0300

    Delete api directory

--- api/relatorios.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import handler from './reports';
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  return handler(req, res);
-} 
