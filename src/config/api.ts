077175d9b574ed5ce35ddd59e9a58fe617b7876c
Wesley <142677140+rattposion@users.noreply.github.com>
2025-06-06 14:26:29 -0300

    Delete src directory

--- src/config/api.ts
+++
@@ -1,7 +0,0 @@
-export const API_URL = 'https://web-production-ac65.up.railway.app';
-
-export const endpoints = {
-  reports: `${API_URL}/api/reports`,
-  stock: `${API_URL}/api/stock`,
-  exits: `${API_URL}/api/exits`
-}; 
